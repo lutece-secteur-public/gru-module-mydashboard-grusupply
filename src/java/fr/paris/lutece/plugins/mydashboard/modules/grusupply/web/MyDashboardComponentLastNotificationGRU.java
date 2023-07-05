1ae8ab55c20e977b61b9151c90322161358226c3
Ben haddi omar <omar.ben.haddi@accenture.com>
2023-07-05 18:29:21 +0200

    Mydashboard grusupply

---
+++ src/java/fr/paris/lutece/plugins/mydashboard/modules/grusupply/web/MyDashboardComponentLastNotificationGRU.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2002-2023, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.mydashboard.modules.grusupply.web;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+
+import fr.paris.lutece.plugins.mydashboard.modules.grusupply.service.NotificationGruService;
+import fr.paris.lutece.plugins.mydashboard.service.MyDashboardComponent;
+import fr.paris.lutece.portal.service.i18n.I18nService;
+import fr.paris.lutece.portal.service.security.LuteceUser;
+import fr.paris.lutece.portal.service.security.SecurityService;
+import fr.paris.lutece.portal.service.template.AppTemplateService;
+import fr.paris.lutece.util.html.HtmlTemplate;
+
+/**
+ * 
+ * MyDashboardComponentLastNotificationGRU
+ *
+ */
+public class MyDashboardComponentLastNotificationGRU extends MyDashboardComponent
+{
+
+    /**
+     * 
+     */
+    private static final long      serialVersionUID                = 8297192924908575568L;
+
+    private static final String    TEMPLATE_LAST_NOTIFICATION_LIST = "skin/plugins/mydashboard/modules/grusupply/dashboard_last_demand.html";
+    private static final String    DASHBOARD_COMPONENT_ID          = "mydashboard-grusupply.componentLastNotification";
+    private static final String    MESSAGE_COMPONENT_DESCRIPTION   = "module.mydashboard.grusupply.myDashboardComponentLastNotification.description";
+
+    // MARKS
+    private static final String    MARK_DEMAND_TYPE_LIST           = "demand_types_list";
+    private static final String    MARK_RESULT                     = "result";
+
+    @Inject
+    @Named( NotificationGruService.BEAN_NAME )
+    private NotificationGruService _notificationService;
+
+    @Override
+    public String getDashboardData( HttpServletRequest request )
+    {
+        LuteceUser user = SecurityService.getInstance( ).getRegisteredUser( request );
+
+        if ( user != null )
+        {
+            Map<String, Object> model = new HashMap<>( );
+
+            model.put( MARK_DEMAND_TYPE_LIST, _notificationService.getListDemandType( ) );
+            model.put( MARK_RESULT, _notificationService.getListDemand( user.getName( ), "1" ) );
+
+            HtmlTemplate htmTemplate = AppTemplateService.getTemplate( TEMPLATE_LAST_NOTIFICATION_LIST, request.getLocale( ), model );
+
+            return htmTemplate.getHtml( );
+        }
+
+        return StringUtils.EMPTY;
+    }
+
+    @Override
+    public String getComponentId( )
+    {
+        return DASHBOARD_COMPONENT_ID;
+    }
+
+    @Override
+    public String getComponentDescription( Locale locale )
+    {
+        return I18nService.getLocalizedString( MESSAGE_COMPONENT_DESCRIPTION, locale );
+    }
+
+}
